Add base pose estimator node class and fix quaternions

pose_estimator/points_pose_estimator_node.py
@@ -1,70 +1,33 @@
 import logging
 
-import cv2
-import numpy as np
 import rclpy
-import tf2_ros
-import tf_transformations
 from bb_perception_msgs.msg import PointCorrespondencesStamped
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from rclpy.node import Node
-from rclpy.wait_for_message import wait_for_message
-from sensor_msgs.msg import CameraInfo
-from transforms3d.quaternions import mat2quat
 from utils.ros_np_multiarray import to_numpy_f64
 
-from pose_estimator.utils.PinholeCamera import PinholeCamera
 from pose_estimator.utils.pose_estimator import (
-    estimate_covariance,
     filter_by_homography,
     get_object_pose,
     refine_object_pose,
 )
-from pose_estimator.utils.ros_messages import (
-    get_pose_with_covariance_stamped,
-    get_transform_stamped,
-)
+from pose_estimator.utils.pose_estimator_node import PoseEstimatorNode
 
 
-class PointsPoseEstimator(Node):
+class PointsPoseEstimator(PoseEstimatorNode):
 
     def __init__(self):
         super().__init__("points_pose_estimator")
 
-        camera_info_topic = (
-            self.declare_parameter("camera_info_topic", "camera_info")
-            .get_parameter_value()
-            .string_value
-        )
         input_points_topic = (
             self.declare_parameter("input_points_topic", "point_correspondences")
             .get_parameter_value()
             .string_value
         )
-        output_pose_topic = (
-            self.declare_parameter("output_pose_topic", "pose")
-            .get_parameter_value()
-            .string_value
-        )
-
-        valid, camera_info = wait_for_message(CameraInfo, self, camera_info_topic)
-        if not valid:
-            raise ValueError("Failed to get camera info")
-        else:
-            camera_info: CameraInfo
-            self.camera = PinholeCamera.from_camera_info(camera_info, rectified=False)
-
-        self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
 
         self.point_subscriber = self.create_subscription(
             PointCorrespondencesStamped,
             input_points_topic,
             self.point_correspondences_callback,
             1,
-        )
-
-        self.pose_publisher = self.create_publisher(
-            PoseWithCovarianceStamped, output_pose_topic, 1
         )
 
     def point_correspondences_callback(self, msg: PointCorrespondencesStamped):
@@ -101,45 +64,11 @@ class PointsPoseEstimator(Node):
                 self.camera, object_points, image_points, rvec, tvec
             )
 
-            R, _ = cv2.Rodrigues(rvec)
-            t = tvec.squeeze()
-
         except Exception as e:
             self.get_logger().warn(f"Pose estimation failed: {e}")
             return
 
-        try:
-            covariance = estimate_covariance(object_points, rvec, tvec, self.camera)
-        except np.linalg.LinAlgError as e:
-            self.get_logger().warn(
-                f"Covariance estimation failed, inversion for FIM matrix failed: {e}"
-            )
-            return
-
-        # self.get_logger().info(
-        #     f"Pose estimation std dev: {np.sqrt(covariance.diagonal())}"
-        # )
-
-        try:
-            q = mat2quat(R)
-        except np.linalg.LinAlgError as e:
-            self.get_logger().warn(f"Error in mat2quat, failed to convert R: {e}")
-            return
-
-        # Apply a 180-degree rotation around the x-axis
-        # TODO: Find out why this is needed
-        q_rot_x_180 = tf_transformations.quaternion_from_euler(np.pi, 0, 0)
-        q_rotated = tf_transformations.quaternion_multiply(q_rot_x_180, q)
-
-        pose = get_pose_with_covariance_stamped(
-            msg.header, t, q_rotated, covariance.flatten().tolist()
-        )
-        self.pose_publisher.publish(pose)
-
-        transform_stamped = get_transform_stamped(
-            msg.header, msg.object_frame_id, t, q_rotated
-        )
-        self.tf_broadcaster.sendTransform(transform_stamped)
+        self.publish_data(tvec, rvec, object_points, msg.header, msg.object_frame_id)
 
 
 def main(args=None):