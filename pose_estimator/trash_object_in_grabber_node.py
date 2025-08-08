3a9276947c12f2aa7f99b82acc312c04631ecc08
SamuelFoo <fooenzesamuel@gmail.com>
2025-08-08 07:47:50 -0700

    Edit trash_object_in_grabber_node

--- pose_estimator/trash_object_in_grabber_node.py
+++ pose_estimator/trash_object_in_grabber_node.py
@@ -1,18 +1,17 @@
+import numpy as np
 import rclpy
-from rclpy.node import Node
-from yolo_msgs.msg import DetectionArray, Detection
-import shapely
 from foxglove_msgs.msgs import ImageAnnotations
-
-from pose_estimator.utils.detections import (
-    get_IoA,
-    get_detection_perimeter,
-    get_top_k_detections_per_class,
-    is_in_polygon,
-    get_detection_polygon
-)
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+from yolo_msgs.msg import DetectionArray
 
 from image_processing.utils.image_annotations import get_image_annotations
+from pose_estimator.utils.detections import (
+    get_detection_polygon,
+    get_IoA,
+    get_top_k_detections_per_class,
+)
+
 
 class TrashObjectInGrabberNode(Node):
     def __init__(self):
@@ -21,47 +20,38 @@ class TrashObjectInGrabberNode(Node):
         input_detections_topic = (
             self.declare_parameter(
                 "input_detections_topic", rclpy.Parameter.Type.STRING
-            ).get_parameter_value()
-            .string_value
-        )
-
-        bottle_in_grabber_topic = (
-            self.declare_parameter(
-                "bottle_in_grabber_topic", rclpy.Parameter.Type.STRING
-            ).get_parameter_value()
-            .string_value
-        )
-
-        ladle_in_grabber_topic = (
-            self.declare_parameter(
-                "ladle_in_grabber_topic", rclpy.Parameter.Type.STRING
             )
             .get_parameter_value()
             .string_value
         )
-
-        bottle_grab_region_polygon_param = (
+        output_annotations_topic = (
             self.declare_parameter(
-                "bottle_grab_region_polygon", rclpy.Parameter.Type.DOUBLE_ARRAY
-            ).get_parameter_value()
+                "output_annotations_topic", rclpy.Parameter.Type.STRING
+            )
+            .get_parameter_value()
+            .string_value
+        )
+        output_objects_in_grabber_topic = (
+            self.declare_parameter(
+                "output_objects_in_grabber_topic", rclpy.Parameter.Type.STRING
+            )
+            .get_parameter_value()
+            .string_value
+        )
+        bottle_roi_polygon = (
+            self.declare_parameter(
+                "bottle_roi_polygon", rclpy.Parameter.Type.DOUBLE_ARRAY
+            )
+            .get_parameter_value()
             .double_array_value
         )
-
-        self.bottle_grab_region_polygon = np.array(bottle_grab_region_polygon_param).reshape(
-            -1, 2
-        )
-
-        ladle_grab_region_polygon_param = (
+        ladle_roi_polygon = (
             self.declare_parameter(
-                "ladle_grab_region_polygon", rclpy.Parameter.Type.DOUBLE_ARRAY
-            ).get_parameter_value()
+                "ladle_roi_polygon", rclpy.Parameter.Type.DOUBLE_ARRAY
+            )
+            .get_parameter_value()
             .double_array_value
         )
-
-        self.ladle_grab_region_polygon = np.array(ladle_grab_region_polygon_param).reshape(
-            -1, 2
-        )
-
         self.bottle_grab_area_threshold = (
             self.declare_parameter(
                 "bottle_grab_area_threshold", rclpy.Parameter.Type.DOUBLE
@@ -75,60 +65,65 @@ class TrashObjectInGrabberNode(Node):
             )
             .get_parameter_value()
             .double_value
-        ) 
-
-        self.bottle_in_grabber_pub = self.create_publisher(
-            DetectionArray, bottle_in_grabber_topic
-        )
-
-        self.ladle_in_grabber_pub = self.create_publisher(
-            DetectionArray, ladle_in_grabber_topic
         )
 
         self.detections_sub = self.create_subscription(
-            DetectionArray, input_detections_topic, self.detections_callback
+            DetectionArray,
+            input_detections_topic,
+            self.detections_callback,
+            qos_profile=qos_profile_sensor_data,
         )
-
-        annotations_topic = (
-            self.declare_parameter(
-                "annotations_topic", rclpy.Parameter.Type.STRING
-            )
-            .get_parameter_value()
-            .string_value
+        self.objects_in_grabber_pub = self.create_publisher(
+            DetectionArray,
+            output_objects_in_grabber_topic,
+            qos_profile=qos_profile_sensor_data,
         )
-
         self.annotations_pub = self.create_publisher(
-            ImageAnnotations, annotations_topic,
+            ImageAnnotations,
+            output_annotations_topic,
+            qos_profile=qos_profile_sensor_data,
         )
 
-    def detetections_callback(self, detection_array_msg: DetectionArray):
+        self.bottle_grab_roi_polygon = np.array(bottle_roi_polygon).reshape(-1, 2)
+        self.ladle_grab_roi_polygon = np.array(ladle_roi_polygon).reshape(-1, 2)
+
+    def detections_callback(self, detection_array_msg: DetectionArray):
         best_detections = get_top_k_detections_per_class(
-            detection_array_msg,
-            {"bottle": 2, "ladle": 2, "pink_bucket": 1, "yellow_bucket": 1},
+            detection_array_msg, {"bottle": 2, "ladle": 2}
         )
 
-        bottle_detection_array = DetectionArray()
-        ladle_detection_array = DetectionArray()
-        for class_name, detection in best_detections.items():
-            if class_name == "bottle":
-                if (get_IoA(get_detection_polygon(detection), self.bottle_grab_region_polygon)) >= self.bottle_grab_area_threshold:
-                    bottle_detection_array.detections.append(detection)
-            elif class_name == "ladle":
-                if (
-                    get_IoA(get_detection_polygon(detection), self.ladle_grab_region_polygon)
-                    >= self.ladle_grab_area_threshold
-                ):
-                    ladle_detection_array.detections.append(detection)
+        detections_in_grabber: list[Detection] = []
+        for class_name, detections in best_detections.items():
+            for det in detections:
+                containing_polygon = (
+                    self.bottle_grab_roi_polygon
+                    if class_name == "bottle"
+                    else self.ladle_grab_roi_polygon
+                )
+                threshold = (
+                    self.bottle_grab_area_threshold
+                    if class_name == "bottle"
+                    else self.ladle_grab_area_threshold
+                )
+                IoA = get_IoA(get_detection_polygon(det), containing_polygon)
+                if IoA >= threshold:
+                    detections_in_grabber.append(det)
 
-        self.bottle_in_grabber_pub.publish(bottle_detection_array) 
-        self.ladle_in_grabber_pub.publish(ladle_detection_array)
+        output_msg = DetectionArray()
+        output_msg.header = detection_array_msg.header
+        output_msg.detections = detections_in_grabber
+        self.objects_in_grabber_pub.publish(output_msg)
+
         image_annotations = get_image_annotations(
             detection_array_msg.header,
             [
-                map(get_detection_polygon, best_detections.values())
-            ]
+                map(get_detection_polygon, detections_in_grabber),
+                [self.bottle_grab_roi_polygon],
+                [self.ladle_grab_roi_polygon],
+            ],
         )
         self.annotations_pub.publish(image_annotations)
+
 
 def main(args=None):
     rclpy.init(args=args)
