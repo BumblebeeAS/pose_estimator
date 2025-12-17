646b30888952c6d794cefdf9743fd2507c553f48
SamuelFoo <fooenzesamuel@gmail.com>
2025-12-17 14:22:31 +0800

    Correct helipad attitude

--- pose_estimator/helipad_ellipse_pose_estimator_node.py
+++ pose_estimator/helipad_ellipse_pose_estimator_node.py
@@ -1,7 +1,19 @@
+import cv2
 import rclpy
+import tf2_ros
 from foxglove_msgs.msg import ImageAnnotations, PointsAnnotation
+from geometry_msgs.msg import Transform
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+from nav_msgs.msg import Odometry
 from rclpy.qos import qos_profile_sensor_data
+from rclpy.time import Time
+from rclpy.wait_for_message import wait_for_message
 from scipy.spatial import ConvexHull
+from tf_transformations import (
+    quaternion_inverse,
+    quaternion_matrix,
+    quaternion_multiply,
+)
 from yolo_msgs.msg import DetectionArray
 
 from image_processing.utils.image_annotations import get_image_annotations
@@ -34,13 +46,46 @@ class HelipadPoseEstimator(PoseEstimatorNode):
             .get_parameter_value()
             .string_value
         )
+        self.odom_ned_topic = (
+            self.declare_parameter("odom_ned", "/uav2/odom_ned")
+            .get_parameter_value()
+            .string_value
+        )
 
-        self.detections_sub = self.create_subscription(
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
+        self.tf_timeout_sec = 1.0
+
+        # Get base_link frame from odom_ned
+        valid, odom_ned_msg = wait_for_message(Odometry, self, self.odom_ned_topic)
+        if not valid:
+            raise ValueError("Failed to get camera info")
+        else:
+            odom_ned_msg: Odometry
+        self.base_link_frame = odom_ned_msg.child_frame_id
+        self.get_logger().info(
+            f"Determined base_link frame as {self.base_link_frame} from odom_ned message."
+        )
+
+        # Creation of subscribers must happen after wait_for_message above
+        self.detections_sub = Subscriber(
+            self,
             DetectionArray,
             input_detections_topic,
-            self.detections_callback,
-            qos_profile_sensor_data,
+            qos_profile=qos_profile_sensor_data,
         )
+        self.odom_ned_sub = Subscriber(
+            self,
+            Odometry,
+            self.odom_ned_topic,
+            qos_profile=qos_profile_sensor_data,
+        )
+        self.time_sync = ApproximateTimeSynchronizer(
+            fs=[self.detections_sub, self.odom_ned_sub],
+            queue_size=200,
+            slop=0.1,
+        )
+        self.time_sync.registerCallback(self.detections_callback)
 
         # Debug annotations publisher
         self.ellipse_annotation_publisher = self.create_publisher(
@@ -54,7 +99,38 @@ class HelipadPoseEstimator(PoseEstimatorNode):
             qos_profile=qos_profile_sensor_data,
         )
 
-    def detections_callback(self, detections_msg: DetectionArray):
+        self.is_setup = False
+
+    def get_base_link_to_camera_transform(self) -> tuple[bool, Transform | None]:
+        camera_frame_id = self.camera.frame_id
+        self.get_logger().info(
+            f"Looking up transform from {self.base_link_frame} to {camera_frame_id}"
+        )
+        if not self.tf_buffer.can_transform(
+            self.base_link_frame, camera_frame_id, Time()
+        ):
+            return False, None
+
+        tf = self.tf_buffer.lookup_transform(
+            self.base_link_frame, camera_frame_id, Time()
+        )
+        return True, tf.transform
+
+    def detections_callback(
+        self, detections_msg: DetectionArray, odom_ned_msg: Odometry
+    ):
+        if not self.is_setup:
+            self.get_logger().info("Setting up helipad pose estimator.")
+            success, base_link_to_camera_tf = self.get_base_link_to_camera_transform()
+            if not success:
+                self.get_logger().warn(
+                    "Failed to get base_link to camera transform. Cannot proceed with pose estimation."
+                )
+                return
+            self.base_link_to_camera_tf = base_link_to_camera_tf
+            self.is_setup = True
+            self.get_logger().info("Successfully set up helipad pose estimator.")
+
         # We require at least 3 points for polygon creation
         filtered_detections = filter_detections_by_num_points(detections_msg, 3)
 
@@ -133,7 +209,18 @@ class HelipadPoseEstimator(PoseEstimatorNode):
             self.get_logger().warn(f"Pose estimation failed: {e}")
             return
 
-        self.publish_transform(tvec, rvec, detections_msg.header, self.object_frame_id)
+        odom_to_base_link_rotation = odom_ned_msg.pose.pose.orientation
+        base_link_to_camera_rotation = self.base_link_to_camera_tf.rotation
+        odom_to_camera_rotation = quaternion_multiply(
+            odom_to_base_link_rotation, base_link_to_camera_rotation
+        )
+        camera_to_odom_rotation = quaternion_inverse(odom_to_camera_rotation)
+        R = quaternion_matrix(camera_to_odom_rotation)[:3, :3]
+        camera_to_odom_rvec, _ = cv2.Rodrigues(R)
+
+        self.publish_transform(
+            tvec, camera_to_odom_rvec, detections_msg.header, self.object_frame_id
+        )
 
 
 def main(args=None):
